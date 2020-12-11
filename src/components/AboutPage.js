fdb94e702d9d9f5d63d5b4c9f63dc7ea7e375570
Jaime Munoz <jmunoz@decibelinsight.com>
2020-12-11 09:53:50 +0100

    AboutPage working without hook

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -13,7 +13,7 @@ const targets = document.querySelectorAll(".item")
 
 targets.forEach(function (target) {
   // Hide the element
-  target.classList.add("opacity-20");
+  target.classList.add("opacity-10");
 
   // Add the element to the watcher
   observer.observe(target);
@@ -23,10 +23,11 @@ targets.forEach(function (target) {
 const listItems = aboutMeData.items.map((item, i) =>
 
   <div key={i}>
-    <div className={`my-80 relative flex h-15 ${i % 2 === 0 ? 'float-right' : 'float-left'} justify-between clear-both w-1/2 item`}
+    <div className={`my-60 relative flex h-15 ${i % 2 === 0 ? 'float-right' : 'float-left'} justify-between clear-both w-1/2`}
       dir={`${i % 2 === 0 ? 'ltr' : 'rtl'}`}>
       <div className="box-border absolute h-6 w-6 rounded-full border-white border-2 bg-blue-200 mt-2 z-50 -m-3"></div>
-      <div className="w-11/12 mx-auto px-4 text-gray-700">
+
+      <div className="w-11/12 mx-auto px-4 text-gray-700  item">
         <h3 className="my-1 text-2xl font-extrabold tracking-tight">
           {item.title}
         </h3>
