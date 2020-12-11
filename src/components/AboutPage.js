f8db259bbece4be8f1a14460bc39cff1d2ba553a
Jaime Munoz <jmunoz@decibelinsight.com>
2020-12-11 10:15:29 +0100

    add useOnScreen hook

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,4 +1,4 @@
-import React, { user } from 'react'
+import React from 'react'
 import aboutMeData from '../data/about-me.json'
 
 const callback = (entries) => {
@@ -20,39 +20,37 @@ targets.forEach(function (target) {
 });
 
 
-const listItems = aboutMeData.items.map((item, i) =>
-
-  <div key={i}>
-    <div className={`my-60 relative flex h-15 ${i % 2 === 0 ? 'float-right' : 'float-left'} justify-between clear-both w-1/2`}
-      dir={`${i % 2 === 0 ? 'ltr' : 'rtl'}`}>
-      <div className="box-border absolute h-6 w-6 rounded-full border-white border-2 bg-blue-200 mt-2 z-50 -m-3"></div>
-
-      <div className="w-11/12 mx-auto px-4 text-gray-700  item">
-        <h3 className="my-1 text-2xl font-extrabold tracking-tight">
-          {item.title}
-        </h3>
-        <h1 className="text-xl font-extrabold tracking-tight">
-          {item.year}
-        </h1>
-      </div>
-    </div>
-  </div>
-
-)
-
-const AboutPage = () => {
-  return (
-    <>
-      <div className="w-full my-20 text-3xl text-center font-extrabold tracking-tight capitalize text-gray-700">
-        scroll down to know more about me
-    </div>
-      <div className="mx-auto relative overflow-hidden m-10 p-10 w-5/6">
-        {listItems}
-        <div className="absolute left-lefty top-0 w-1 h-full bg-gray-200 z-10">
+const ListItems = () => (
+  aboutMeData.items.map((item, i) =>
+    <div key={i}>
+      <div className={`my-60 relative flex h-15 ${i % 2 === 0 ? 'float-right' : 'float-left'} justify-between clear-both w-1/2`}
+        dir={`${i % 2 === 0 ? 'ltr' : 'rtl'}`}>
+        <div className="box-border absolute h-6 w-6 rounded-full border-white border-2 bg-blue-200 mt-2 z-50 -m-3"></div>
+        <div className="w-11/12 mx-auto px-4 text-gray-700  item">
+          <h3 className="my-1 text-2xl font-extrabold tracking-tight">
+            {item.title}
+          </h3>
+          <h1 className="text-xl font-extrabold tracking-tight">
+            {item.year}
+          </h1>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  ))
+
+
+const AboutPage = () => (
+  <>
+    <div className="w-full my-20 text-3xl text-center font-extrabold tracking-tight capitalize text-gray-700">
+      scroll down to know more about me
+    </div>
+    <div className="mx-auto relative overflow-hidden m-10 p-10 w-5/6">
+      <ListItems />
+      <div className="absolute left-lefty top-0 w-1 h-full bg-gray-200 z-10">
+      </div>
+    </div>
+  </>
+)
+
 
 export default AboutPage
