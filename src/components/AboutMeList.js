f347c0b018082f91714640a99388f17815a59e0e
Jaime Munoz <jaimungon@gmail.com>
2020-12-14 15:22:01 +0100

    homepage / AboutMeList finished

--- src/components/AboutMeList.js
+++ src/components/AboutMeList.js
@@ -1,13 +1,52 @@
 import React from 'react'
 
+const icons = [
+  {
+    name: "university",
+    path: '<path fill="#fff" d="M12 14l9-5-9-5-9 5 9 5z" /><path fill="#fff" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" /> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />',
+    desciption: "Studied Finance at Sussex Uni"
+  },
+  {
+    name: "",
+    path: '<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />',
+    desciption: "Self-taught Coder"
+  },
+  {
+    name: "",
+    path: '<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />',
+    desciption: "Love to Work in Tech"
+  },
+  {
+    name: "",
+    path: '<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />',
+    desciption: "Backpacked the World for 3 Years"
+  },
+  {
+    name: "",
+    path: ' <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />',
+    desciption: "Quick Learner"
+  },
+  {
+    name: "",
+    path: '<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />',
+    desciption: "Can Talk 3 Languages"
+  },
+]
+
+
+const itemsList = icons.map((icon, i) =>
+  <div key={i} className="w-36">
+    <svg className="mx-auto h-16 w-16 text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" dangerouslySetInnerHTML={{ __html: icon.path }}></svg>
+    <h2 className="text-gray-800 text-center tracking-tight font-bold">
+      {icon.desciption}
+    </h2>
+  </div>
+)
+
 const AboutMeList = () => {
   return (
-    <div>
-      <svg className="h-16 w-16" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path fill="#fff" d="M12 14l9-5-9-5-9 5 9 5z" />
-        <path fill="#fff" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
-      </svg>
+    <div className="py-10 mx-auto max-w-screen-md items-center justify-items-center grid grid-cols-2 md:grid-cols-3 gap-4">
+      {itemsList}
     </div>
   )
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -14,44 +14,47 @@ const HomePage = () => {
       </div>
       <div className="text-gray-700 m-auto max-w-screen-sm sm:py-10">
         <p className="font-bold sm:text-xl text-center m-10 sm:py-4">
-          Hi 🙋🏽‍♂️ my name is Jay, I build experiences in the web.
+          Hi 🙋🏽‍♂️ I am Jay, I build experiences for the web.
         </p>
         <p className="font-bold sm:text-xl text-center m-10 sm:py-4">
           I've been working in the digital sector since 2013 when I built my first website. I have also created a ton of AB tests and analytics implementations.
         </p>
         <p className="font-bold sm:text-xl text-center m-10 sm:py-4">
-          Now I do Front End development with React and learning Back End with the Express and MongoDB.
+          Now I do Front End development with React and Tailwind. I'm also learning Back End with Express and MongoDB.
         </p>
         <p className="font-bold sm:text-xl text-center m-10 sm:pt-4">
           Bellow are a few examples of my work and ways to contact.
         </p>
       </div>
       <div className="h-40 w-full bg-gradient-to-r from-cyan-300 via-teal-400 to-blue-500 shadow-lg rounded-b-lg my-6 sm:my-10"></div>
-      <div className="max-w-screen-sm mx-auto">
-        <p className="text-4xl text-center tracking-tight text-gray-700 font-extrabold py-10 sm:py-20">
+      <div className="max-w-screen-sm mx-auto text-gray-700">
+        <p className="text-4xl text-center tracking-tight font-extrabold py-10 sm:py-20">
           Current Projects
         </p>
-        <p className="font-bold sm:text-xl text-justify px-10 text-gray-700">
-          This is a small list of projects that I'm currently working on, if you would like to have a look at the full list please click <a className="text-blue-400 hover:text-gray-400 italic" href="/projects">here </a> or go to the Projects tab
+        <p className="font-bold sm:text-xl text-justify px-10">
+          This is a small list of projects that I'm currently working on, if you would like to have a look at the full list please click <a className="text-blue-400 hover:text-gray-400 italic" href="/projects">HERE </a> to go to the Projects tab
         </p>
       </div>
 
       <div className="max-w-screen-md mx-auto sm:py-10 grid sm:grid-cols-2 grid-cols-1 gap-4">
         < ListCardComponent projects={projects.slice(0, 4)} />
       </div>
-      <div className="h-40 w-full bg-gradient-to-r from-cyan-300 via-teal-400 to-blue-500 shadow-lg rounded-b-lg my-6 sm:my-10"></div>
-      <div className="max-w-screen-sm mx-auto">
-        <p className="text-4xl text-center tracking-tight text-gray-700 font-extrabold py-10 sm:py-20">
+      <div className="h-40 w-full bg-gradient-to-l from-cyan-300 via-teal-400 to-blue-500 shadow-lg rounded-b-lg my-6 sm:my-10"></div>
+      <div className="max-w-screen-sm mx-auto text-gray-700">
+        <p className="text-4xl text-center tracking-tight font-extrabold py-10 sm:py-20">
           About Me
         </p>
-        <p className="font-bold sm:text-xl content-center px-10">
-          This section still on the making, if you want to know more about me, please click <a className="text-blue-400 hover:text-gray-400 italic" href="/about">here </a> or go to the About Me tab
-        </p>
-        {/* <AboutMeList /> */}
+        <AboutMeList />
+        <div className="font-bold sm:text-xl text-center p-10 sm:py-20">
+          <p>
+            To see a full list of live events click <a className="text-blue-400 hover:text-gray-400 italic" href="/about"> HERE </a></p>
+          <p className="pt-10 sm:pt-20">For a recruiter ready CV click <a className="text-blue-400 hover:text-gray-400 italic" href="/cv">HERE </a>
+          </p>
+        </div>
       </div>
-      <div className="h-40 w-full bg-gradient-to-l from-cyan-300 via-teal-400 to-blue-500 shadow-lg rounded-b-lg my-6 sm:my-10"></div>
-      <div className="max-w-screen-sm mx-auto">
-        <p className="text-4xl text-center tracking-tight text-gray-700 font-extrabold py-10 sm:py-20">
+      <div className="h-40 w-full bg-gradient-to-r from-cyan-300 via-teal-400 to-blue-500 shadow-lg rounded-b-lg my-6 sm:my-10"></div>
+      <div className="max-w-screen-sm mx-auto text-gray-700">
+        <p className="text-4xl text-center tracking-tight font-extrabold py-10 sm:py-20">
           Contact Me
         </p>
         <p className="font-bold sm:text-xl content-center px-10 text-justify">
