f3ceb7c37d76d4358be693c163c39716607a2f0d
Jay M Gonzalez <jaimungon@gmail.com>
2022-03-26 02:32:03 +0100

    add new CV + download pdf functionality

--- src/components/CVPage.js
+++ src/components/CVPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import img from '../img/JMG-CV.png'
+import pdf from '../data/JMG-DO.pdf'
 
 const CVPage = () => {
   return (
     <div>
-      <img className="mx-auto" src={img} alt="My CV" />
+      <a download="Jay-M-Gonzalez-CV.pdf" href={pdf} target="_blank" rel="noreferrer">
+        <img className="mx-auto" src={img} alt="My CV" />
+      </a>
     </div>
   )
 }
