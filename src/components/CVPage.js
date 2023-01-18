4d82a67f558aef426bc45c5eea5904f8737e8493
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-18 15:53:04 +0100

    add pdf cv

--- src/components/CVPage.js
+++ src/components/CVPage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import img from '../img/JMG-CV.png'
-import pdf from '../data/JMG-DO.pdf'
+import pdf from '../data/JMG-CV.pdf'
 
 const CVPage = () => {
   return (
     <div>
-      <a download="Jay-M-Gonzalez-CV.pdf" href={pdf} target="_blank" rel="noreferrer">
+      <a
+        download="Jay-M-Gonzalez-CV.pdf"
+        href={pdf}
+        target="_blank"
+        rel="noreferrer"
+      >
         <img className="mx-auto" src={img} alt="My CV" />
       </a>
     </div>
